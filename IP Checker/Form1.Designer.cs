d4f235a220180f8c012681e615ee4956e51a939c
S a m e r <S a m e r>
2016-05-29 07:55:30 +0200

    first commit

---
+++ IP Checker/Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace IP_Checker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.maskedTextBox1 = new System.Windows.Forms.MaskedTextBox();
+            this.maskedTextBox2 = new System.Windows.Forms.MaskedTextBox();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(21, 17);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(445, 134);
+            this.listBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(18, 164);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(31, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "From";
+            // 
+            // label2
+            // 
+            this.label2.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(179, 164);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(19, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "To";
+            // 
+            // button1
+            // 
+            this.button1.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.button1.Location = new System.Drawing.Point(334, 157);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(132, 24);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Enter";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // maskedTextBox1
+            // 
+            this.maskedTextBox1.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.maskedTextBox1.InsertKeyMode = System.Windows.Forms.InsertKeyMode.Overwrite;
+            this.maskedTextBox1.Location = new System.Drawing.Point(50, 161);
+            this.maskedTextBox1.Mask = "000.000.000.000";
+            this.maskedTextBox1.Name = "maskedTextBox1";
+            this.maskedTextBox1.Size = new System.Drawing.Size(123, 20);
+            this.maskedTextBox1.TabIndex = 6;
+            this.maskedTextBox1.KeyUp += new System.Windows.Forms.KeyEventHandler(this.maskedTextBox1_KeyUp);
+            // 
+            // maskedTextBox2
+            // 
+            this.maskedTextBox2.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.maskedTextBox2.InsertKeyMode = System.Windows.Forms.InsertKeyMode.Overwrite;
+            this.maskedTextBox2.Location = new System.Drawing.Point(204, 161);
+            this.maskedTextBox2.Mask = "099.099.099.099";
+            this.maskedTextBox2.Name = "maskedTextBox2";
+            this.maskedTextBox2.Size = new System.Drawing.Size(123, 20);
+            this.maskedTextBox2.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.ClientSize = new System.Drawing.Size(490, 194);
+            this.Controls.Add(this.maskedTextBox2);
+            this.Controls.Add(this.maskedTextBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox1);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox1;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox2;
+    }
+}
+
---
+++ IP Checker/Form1.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Net.NetworkInformation;
+using System.Threading;
+using System.Net;
+
+
+namespace IP_Checker
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Thread t = new Thread(Write);
+            t.IsBackground = true;
+            if (button1.Text == "Enter")
+            {
+                t.Start();
+                CheckForIllegalCrossThreadCalls = false;
+                button1.Text = "Stop";
+            }
+            else
+            {
+                button1.Text = "Enter";
+                t.Abort();
+            }
+
+        }
+
+        private IPAddress increament(IPAddress ip)
+        {
+            string[] i = ip.ToString().Split('.');
+
+            if (Convert.ToInt32(i[3]) < 255)
+                return IPAddress.Parse(i[0] + '.' + i[1] + '.' + i[2] + '.' + (Convert.ToInt32(i[3]) + 1).ToString());
+
+            if (Convert.ToInt32(i[2]) < 255)
+                return IPAddress.Parse(i[0] + '.' + i[1] + '.' + (Convert.ToInt32(i[2]) + 1).ToString() + '.' + "0");
+
+            if (Convert.ToInt32(i[1]) < 255)
+                return IPAddress.Parse(i[0] + '.' + (Convert.ToInt32(i[1]) + 1).ToString() + '.' + "0" + '.' + "0");
+
+            
+                return IPAddress.Parse((Convert.ToInt32(i[0]) + 1).ToString() + '.' + "0" + '.' + "0" + '.' + "0");
+
+        }
+
+        private void Write()
+        {
+
+            while (true)
+            {
+
+
+                listBox1.Items.Clear();
+                string f, l;
+                f = maskedTextBox1.Text.Replace(" ", "");
+                l = maskedTextBox2.Text.Replace(" ", "");
+
+                IPAddress ipf = IPAddress.Parse(f);
+                IPAddress ipl = IPAddress.Parse(l);
+                List<IPAddress> ips = new List<IPAddress>();
+
+                //Show IP
+                IPHostEntry IPHost = Dns.GetHostByName(Dns.GetHostName());
+                listBox1.Items.Add("My IP address is " + IPHost.AddressList[0].ToString());
+
+                //Ping using ping class
+                ips.Add(ipf);
+                IPAddress iph = ipf;
+                while (true)
+                {
+                    iph = increament(iph);
+                    if (iph.Address == ipl.Address)
+                        break;
+                    //     iph.Address = iph.Address + 0x1 <<1;
+                    ips.Add(iph);
+                }
+
+
+                ips.Add(ipl);
+                foreach (IPAddress ip in ips)
+                {
+
+                    Ping pi = new Ping();
+                    PingReply pr = pi.Send(ip);
+
+                    Dns.BeginResolve(ip.ToString(), new AsyncCallback(bringname), ip.ToString() + "   " + pr.Status.ToString());
+                }
+                Thread.Sleep(500);
+            }
+        }
+
+        private void bringname(IAsyncResult ar)
+        {
+
+            IPHostEntry ipe = Dns.EndResolve(ar);
+
+            listBox1.Items.Add((string)ar.AsyncState + " " + ipe.HostName);
+
+        }
+
+       
+        private void maskedTextBox1_KeyUp(object sender, KeyEventArgs e)
+        {
+            maskedTextBox2.Text = maskedTextBox1.Text;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            maskedTextBox1.Text = Dns.GetHostByName(Dns.GetHostName()).AddressList[0].ToString();
+           
+        }
+    }
+
+}
